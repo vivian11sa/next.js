c12bd4fecd1d7c37460ffcd7089d9ef9c4511c8e
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-03-10 15:04:29 +0100

    Store actions entry for RSC

--- crates/next-custom-transforms/src/transforms/server_actions.rs
+++ crates/next-custom-transforms/src/transforms/server_actions.rs
@@ -187,10 +187,15 @@ pub fn server_actions<C: Comments>(
 
 /// Serializes the Server Actions into a magic comment prefixed by
 /// `__next_internal_action_entry_do_not_use__`.
-fn generate_server_actions_comment(actions: ActionsMap) -> String {
+fn generate_server_actions_comment(actions: &ActionsMap, entry: Option<&str>) -> String {
     format!(
         " __next_internal_action_entry_do_not_use__ {} ",
-        serde_json::to_string(&actions).unwrap()
+        if let Some(entry) = entry {
+            serde_json::to_string(&(actions, entry))
+        } else {
+            serde_json::to_string(&actions)
+        }
+        .unwrap()
     )
 }
 
@@ -2116,7 +2121,14 @@ impl<C: Comments> VisitMut for ServerActions<C> {
                     Comment {
                         span: DUMMY_SP,
                         kind: CommentKind::Block,
-                        text: generate_server_actions_comment(actions).into(),
+                        text: generate_server_actions_comment(
+                            &actions,
+                            match self.mode {
+                                ServerActionsMode::Webpack => None,
+                                ServerActionsMode::Turbopack => Some(""),
+                            },
+                        )
+                        .into(),
                     },
                 );
             } else {
@@ -2127,7 +2139,7 @@ impl<C: Comments> VisitMut for ServerActions<C> {
                             Comment {
                                 span: DUMMY_SP,
                                 kind: CommentKind::Block,
-                                text: generate_server_actions_comment(actions).into(),
+                                text: generate_server_actions_comment(&actions, None).into(),
                             },
                         );
                         new.push(client_layer_import.unwrap());
@@ -2170,7 +2182,8 @@ impl<C: Comments> VisitMut for ServerActions<C> {
                                                 span: DUMMY_SP,
                                                 kind: CommentKind::Block,
                                                 text: generate_server_actions_comment(
-                                                    std::iter::once((ref_id, export)).collect(),
+                                                    &std::iter::once((ref_id, export)).collect(),
+                                                    Some(&self.file_name),
                                                 )
                                                 .into(),
                                             },

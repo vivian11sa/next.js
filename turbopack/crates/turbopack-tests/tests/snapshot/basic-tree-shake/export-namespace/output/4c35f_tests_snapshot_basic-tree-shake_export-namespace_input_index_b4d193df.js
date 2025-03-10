6e47a3b5317d3be837dbe95b001c36e3d49a67e0
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-10 13:42:24 +0900

    Update test refs

---
+++ turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/export-namespace/output/4c35f_tests_snapshot_basic-tree-shake_export-namespace_input_index_b4d193df.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/4c35f_tests_snapshot_basic-tree-shake_export-namespace_input_index_b4d193df.js",
+    {},
+    {"otherChunks":["output/b1abf_turbopack-tests_tests_snapshot_basic-tree-shake_export-namespace_input_63c70ee6._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/export-namespace/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
---
+++ turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/require-side-effect/output/4c35f_tests_snapshot_basic-tree-shake_require-side-effect_input_index_98ac8173.js
@@ -0,0 +1,6 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "output/4c35f_tests_snapshot_basic-tree-shake_require-side-effect_input_index_98ac8173.js",
+    {},
+    {"otherChunks":["output/4c35f_tests_snapshot_basic-tree-shake_require-side-effect_input_d8e92216._.js"],"runtimeModuleIds":["[project]/turbopack/crates/turbopack-tests/tests/snapshot/basic-tree-shake/require-side-effect/input/index.js [test] (ecmascript)"]}
+]);
+// Dummy runtime
